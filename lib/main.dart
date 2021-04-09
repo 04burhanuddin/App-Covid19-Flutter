af4ecee3b05b32619862c65bddce8fac730f2171
04burhanuddin <dev.burhanuddin@gmail.com>
2021-04-09 20:13:49 +0700

    update comment

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
+  // mengatur agar tampilan tetap mode potrait
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
@@ -14,11 +15,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // menghilangkan banner debug
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      // halaman yang pertama kali di jalankan adalah
+      // splashScreen
       home: SplashScreen(),
     );
   }
--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -36,6 +36,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // mengganti background
       backgroundColor: background,
       appBar: AppBar(
         title: Text("Covid Indonesia"),
